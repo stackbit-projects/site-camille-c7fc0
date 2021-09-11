a162e1f3a4c4f8b424972503d52ef739dd8aa802
Stackbit Code Editor <projects@stackbit.com>
2021-09-11 10:36:28 +0000

    blog.js: changed

--- src/layouts/blog.js
+++ src/layouts/blog.js
@@ -19,7 +19,7 @@ export default class Blog extends React.Component {
 
         return (
             <Layout page={page} config={config}>
-               { this.props.data.blocks.header.title}
+               { this.props.data}
 
                 <header
                     className={classNames('section', 'section--header', {
